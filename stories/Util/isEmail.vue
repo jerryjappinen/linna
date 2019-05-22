d8eec191abf7d41d3b676091b488e9eb99345539
Jerry Jäppinen <eiskis@gmail.com>
2019-05-22 16:33:31 +0200

    isEmail

---
+++ stories/Util/isEmail.vue
@@ -0,0 +1,37 @@
+<script>
+import isEmail from '../../util/isEmail'
+
+export default {
+
+  data () {
+    return {
+      demo: 'foo@bar.io'
+    }
+  },
+
+  computed: {
+
+    demoIsEmail () {
+      return isEmail(this.demo)
+    }
+
+  }
+
+}
+
+</script>
+
+<template>
+  <div>
+
+    <h1><code>isEmail(string)</code></h1>
+
+    <js-block source="import isEmail from 'linna/util/isEmail'" />
+
+    <p>
+      <input v-model="demo">
+      {{ demoIsEmail }}
+    </p>
+
+  </div>
+</template>

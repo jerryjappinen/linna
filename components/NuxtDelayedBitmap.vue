0b9656de9411442e148b6d019ef1a69f66991d7e
Jerry Jäppinen <eiskis@gmail.com>
2019-12-17 18:48:28 +0200

    NuxtDelayedBitmap

---
+++ components/NuxtDelayedBitmap.vue
@@ -0,0 +1,31 @@
+<script>
+import Bitmap from './Bitmap'
+
+let ImageComponent = Bitmap
+if (process.browser) {
+  ImageComponent = require('./DelayedBitmap').default
+}
+
+export default {
+  name: 'NuxtDelayedBitmap',
+
+  components: {
+    ImageComponent
+  },
+
+  props: {
+    src: {},
+    title: {},
+    hidden: {}
+  }
+
+}
+</script>
+
+<template>
+  <image-component
+    :src="focus"
+    :title="hidden"
+    :hidden="hidden"
+  />
+</template>

158e524b2a1aadf160430cfb34a741ae94c1d493
Jerry Jäppinen <eiskis@gmail.com>
2019-12-20 21:20:44 +0200

    Breakpoints

--- config/styles.js
+++ config/styles.js
@@ -1,12 +1,17 @@
 // Duplicate SCSS values needed by JS
 // NOTE: there are webpack loaders for exposing these from SCSS
 // They're unreliable though and not worth the hassle (especially with Jest)
+
+const breakpointSmall = 800
+const breakpointMedium = 960
+const breakpointLarge = 1280
+
 export default {
   transitionFast: 150,
   transitionSlow: 450,
-  breakpointPhone: 800,
-  breakpointTablet: 1120,
-  breakpointSmall: 768,
-  breakpointMedium: 960,
-  breakpointLarge: 1280
+  breakpointPhone: breakpointSmall,
+  breakpointTablet: breakpointLarge,
+  breakpointSmall,
+  breakpointMedium,
+  breakpointLarge
 }

83eb0e128c7ed19f21d01331631fc21cf98fd749
Jerry Jäppinen <eiskis@gmail.com>
2020-02-12 18:28:43 +0200

    Buggy import

--- components/contentful/NuxtDelayedContentfulImage.vue
+++ components/contentful/NuxtDelayedContentfulImage.vue
@@ -1,7 +1,7 @@
 <script>
 import ContentfulImage from './ContentfulImage'
 
-import isClient from '../util/isClient'
+import isClient from '../../util/isClient'
 
 let imageComponent = ContentfulImage
 if (isClient()) {

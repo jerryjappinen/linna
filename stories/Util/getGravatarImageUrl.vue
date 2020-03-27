5f6e0550361463c6e6f2cb3f763cd9d41944df3a
Jerry Jäppinen <eiskis@gmail.com>
2020-03-27 22:32:30 +0200

    Gravatar utils

---
+++ stories/Util/getGravatarImageUrl.vue
@@ -0,0 +1,58 @@
+<script>
+import getGravatarImageUrl from '../../util/getGravatarImageUrl.js'
+
+export default {
+
+  data () {
+    return {
+      email: 'eiskis@gmail.com',
+      size: 128
+    }
+  },
+
+  computed: {
+
+    imageUrl () {
+      return getGravatarImageUrl(this.email, this.size)
+    }
+
+  }
+
+}
+
+</script>
+
+<template>
+  <div>
+
+    <h1>Gravatar</h1>
+
+    <p>
+      Dependencies: <code>md5</code>
+    </p>
+
+    <blockquote><code>npm install md5</code></blockquote>
+
+    <h3>
+      <code>getGravatarImageUrl</code>
+    </h3>
+
+    <js-block source="getGravatarImageUrl(email, size)" />
+
+    <p>
+
+      <input v-model="email">
+
+      <input
+        v-model="size"
+        type="number"
+      >
+
+    </p>
+
+    <p>
+      <img :src="imageUrl">
+    </p>
+
+  </div>
+</template>

eedc14cdbb32f5b7208e6940e0dfb87537c71582
Jerry Jäppinen <eiskis@gmail.com>
2020-07-08 11:54:28 +0300

    Placeholder components

---
+++ stories/Components/PlaceholderImage.vue
@@ -0,0 +1,45 @@
+<script>
+import PlaceholderImage from '../../components/PlaceholderImage'
+
+export default {
+  components: {
+    PlaceholderImage
+  }
+}
+</script>
+
+<template>
+  <div>
+
+    <h1>PlaceholderImage</h1>
+
+    <p>
+      Render a blank placeholder image tag with specific dimensions. Useful for rendering placeholders while images are being loaded.
+    </p>
+
+    <placeholder-image
+      :width="20"
+      :height="20"
+    />
+
+    <placeholder-image
+      :width="100"
+      :height="20"
+    />
+
+    <placeholder-image
+      :width="100"
+      :height="20"
+      :dpi="2"
+    />
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+.c-placeholder-image {
+  background-color: rgba(0, 0, 0, 0.2);
+}
+
+</style>
---
+++ stories/Components/PlaceholderText.vue
@@ -0,0 +1,45 @@
+<script>
+import PlaceholderText from '../../components/PlaceholderText'
+
+export default {
+  components: {
+    PlaceholderText
+  }
+}
+</script>
+
+<template>
+  <div>
+
+    <h1>PlaceholderText</h1>
+
+    <p>
+      Render a blank placeholder image tag with specific dimensions. Useful for rendering placeholders while images are being loaded.
+    </p>
+
+    <placeholder-text />
+
+    <placeholder-text :words="40" />
+
+    <ul>
+      <li><placeholder-text :inline="true" /></li>
+      <li><placeholder-text :inline="true" /></li>
+      <li><placeholder-text :inline="true" /></li>
+      <li><placeholder-text :inline="true" /></li>
+    </ul>
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+.c-placeholder-text {
+  color: rgba(255, 0, 0, 0.1);
+
+  &:hover {
+    color: rgba(255, 0, 0, 0.5);
+  }
+
+}
+
+</style>
